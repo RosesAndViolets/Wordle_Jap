859fad87af42fd2dd8092d578d502a12b6896ccc
RosesAndViolets <61640805+RosesAndViolets@users.noreply.github.com>
2022-11-06 05:19:27 +0900

    Japanese  Wordle commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { testDictionary, realDictionary } from "./dictionary.js";
+// import * as wanakana from 'wanakana';
 
 // testing
-console.log('test dictionary:', testDictionary);
-
-const dictionary = testDictionary;
+console.log('real dictionary:', realDictionary);
+const dictionary = realDictionary;
 const state = {
   secret: dictionary[Math.floor(Math.random() * dictionary.length)],
   grid: Array(6)
@@ -17,8 +17,7 @@ function updateGrid() {
   for (let i = 0; i < state.grid.length; i++) {
     for (let j = 0; j < state.grid[i].length; j++) {
       const box = document.getElementById(`box${i}${j}`);
-      box.textContent = state.grid[i][j];
-    }
+      box.textContent = state.grid[i][j];}
   }
 }
 
@@ -45,24 +44,63 @@ function drawGrid(container){
 }
 function registerKeyboardEvents(){
     document.body.onkeydown = (e) => {
+        
         const key = e.key;
+        console.log(key);
+
         if (key === 'Enter') {
             if (state.currentCol === 5){
-                const word = getCurrentWord(); //TODO
+                for (let i = 0; i < 5; i++){
+                    document.getElementById(`box${state.currentRow}${i}`).classList.remove('popAnimated');
+                }
+                const word = getCurrentWord();
                 if (isWordValid(word)){
                     revealWord(word);
                     state.currentRow++;
                     state.currentCol = 0;
                 }else{
-                    alert('Not a Valid word.');
+                    alert('登録されていない単語です');
                 }
             }
         }
         if (key === 'Backspace') {
             removeLetter(); //TODO
         }
-        if (isLetter(key)) {
-            addLetter(key); //TODO
+        if (state.currentCol === 5) return;
+        if (isVowel(key)) {
+            var boxContent = state.grid[state.currentRow][state.currentCol]
+            // if current column box text content is empty, add letter
+            if ( boxContent=== ''){
+            addLetter(key);
+            }
+            else{
+                boxContent += key;
+                addLetter(boxContent);
+            }
+        }else if (isConsonant(key)){
+            var boxContent = state.grid[state.currentRow][state.currentCol]
+            // んの時
+            if (boxContent === 'n' && key === 'n'){
+                addLetter('n');
+            }
+            // っの時
+            else if(isConsonant(boxContent) && boxContent === key){
+                addLetter('っ');
+                addKey(key);
+            }
+            // ゃょゅの時
+            else if (isConsonant(boxContent) && key === 'y' ){
+                boxContent += key;
+                addKey(boxContent);
+            }
+            // chの時 except ち
+            else if ((boxContent === 'c' || boxContent === 's') && key === 'h'){
+                boxContent += key;
+                addKey(boxContent);
+            }
+            else{
+                addKey(key);
+            }
         }
         updateGrid();
     }
@@ -102,27 +140,77 @@ function revealWord(guess){
 
     setTimeout(() => {
         if (isWinner){
-            alert('You win!');
+            alert('正解です！ \nおめでとうございます！');
         }else if(isGameOver){
-            alert('Game Over!');
+            alert('ゲームオーバー! 答えは \n\n' + state.secret　+ '\n\nでした');
         }
     }, 3 * animation_duration);
 }
 
-function isLetter(key){
-    return key.length === 1 && key.match(/[a-z]/i);
+function isVowel(key){
+    if(key.length === 1 && key.match(/[aeiou]/i)){return true;}
+    else{return false;
+    }
 }
 
+function isConsonant(key){
+    if(key.length === 1 && key.match(/[bcdfghjklmnpqrstvwxyz]/i)){return true;}
+    else{return false;
+    }
+}
 function addLetter(letter){
+    const row = state.currentRow;
+    const col = state.currentCol;
+    const box = document.getElementById(`box${row}${col}`);
+
     if (state.currentCol === 5) return;
-    state.grid[state.currentRow][state.currentCol] = letter;
+    // if length of letter is >2, tohiragana only the first letter
+    const hira= wanakana.toHiragana(letter);
+    if (hira.length > 1){
+        if(state.currentCol > 3){
+            return;
+        }
+    state.grid[state.currentRow][state.currentCol] = hira[0];
     state.currentCol++;
+    state.grid[state.currentRow][state.currentCol] = hira[1];
+    state.currentCol++;
+    const box1 = document.getElementById(`box${row}${col+1}`);
+    box1.classList.add('popAnimated');
+
+    }else{
+    state.grid[state.currentRow][state.currentCol] = wanakana.toHiragana(letter);
+    state.currentCol++;
+    }
+    box.classList.add('popAnimated');
+}
+
+function addKey(key){
+    const row = state.currentRow;
+    const col = state.currentCol;
+    const box = document.getElementById(`box${row}${col}`);
+
+    if (state.currentCol === 5) return;
+    state.grid[state.currentRow][state.currentCol] = key;
+
 }
 
 function removeLetter(){
+    // check if current box has text content
+    if(state.grid[state.currentRow][state.currentCol] !== '' && state.currentCol !== 5){
+        state.grid[state.currentRow][state.currentCol] = '';
+        return;
+    }
+    
+    const row = state.currentRow;
+    const col = state.currentCol - 1;
+    const box = document.getElementById(`box${row}${col}`);
+
+
+
     if (state.currentCol === 0) return;
-    state.grid[state.currentRow][state.currentCol - 1] = '';
+    state.grid[row][col] = '';
     state.currentCol--;
+    box.classList.remove('popAnimated');
 }
 
 function startup() {
